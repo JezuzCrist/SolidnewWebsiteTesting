18e2fd6b387a0502a27266411759ab12018f8c0b
Jezuz <vfxlord@gmail.com>
2013-06-23 21:29:32 +0300

    same as last

---
+++ solid/js/code/scrollManager.js
@@ -0,0 +1,17 @@
+﻿(function() {
+  $(document).ready(function() {
+    return $('div[scroolto^="#"]').each(function(index) {
+      return $(this).click(function(event) {
+        var target;
+        event.preventDefault();
+        target = $($(this).attr("scroolto"));
+        console.log($(this).attr("scroolto"));
+        return $('html, body').stop().animate({
+          'scrollTop': target.offset().top
+        }, 900, 'swing', function() {
+          return window.location.hash = target;
+        });
+      });
+    });
+  });
+}).call(this);
